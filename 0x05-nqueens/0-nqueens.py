91f9fc80250745d3fcae9f9db281b99583bf25a9
Gilfoye-CentriX <125440612+Gilfoye-CentriX@users.noreply.github.com>
2024-04-11 17:45:15 +0200

    command-line argument

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,57 +1,84 @@
-#!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col, N):
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-    
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    
-    # Check upper diagonal on right side
-    for i, j in zip(range(row, -1, -1), range(col, N)):
-        if board[i][j] == 1:
-            return False
-    
-    return True
+def generate_solutions(row, column):
+    """
+    solve a simple N x N matrix
+    Args:
+        row (int): Number of rows
+        column (int): Number of columns
+    Returns:
+        returns a list of lists
+    """
+    solution = [[]]
+    for queen in range(row):
+        solution = place_queen(queen, column, solution)
+    return solution
 
-def solve_n_queens(N):
+
+def place_queen(queen, column, prev_solution):
+    """
+    Place the queen at a certain position
+    Args:
+        queen (int): The queen
+        column (int): The column to move
+        prev_solution (list): the previous move
+    Returns:
+        returns a list
+    """
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
+
+
+def is_safe(q, x, array):
+    """
+    check if it's safe to make a move
+    Args:
+        q (int): row to move to
+        x (int): column to move to
+        array (array): the matrix
+    Returns:
+        returns a boolean
+    """
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
+
+
+def n_queens(N):
+    """
+    The main entry point
+    Args:
+        N (int): The board size (N x N)
+    Returns:
+        returns None
+    """
+    solutions = generate_solutions(N, N)
+    for array in solutions:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solutions = []
 
-    def solve_util(row):
-        if row == N:
-            solutions.append([row for row in board])
-            return
-        
-        for col in range(N):
-            if is_safe(board, row, col, N):
-                board[row][col] = 1
-                solve_util(row + 1)
-                board[row][col] = 0
-    
-    solve_util(0)
-    
-    for solution in solutions:
-        print(solution)
-
-if __name__ == "__main__":
-    while True:
-        try:
-            N = int(input("Enter the number of queens (N >= 4): "))
-            if N < 4:
-                print("N must be at least 4")
-            else:
-                break
-        except ValueError:
-            print("N must be a number")
-    
-    solve_n_queens(N)
+    n_queens(N)
