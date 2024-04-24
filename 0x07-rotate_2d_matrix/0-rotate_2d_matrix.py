7eebab0799815af951b28a082a3202af945762c4
Gilfoye-CentriX <125440612+Gilfoye-CentriX@users.noreply.github.com>
2024-04-24 16:17:23 +0200

    2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""2D matrix rotation module.
+"""
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    
+    """ Transpose the matrix
+    """
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    """Reverse each row to rotate 90 degrees clockwise
+    """
+    for i in range(n):
+        for j in range(n // 2):
+            matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
+
+    """Output the rotated matrix in the desired format
+    """
+    print('[')
+    for row in matrix:
+        print(f'    {row},')
+    print(']')
